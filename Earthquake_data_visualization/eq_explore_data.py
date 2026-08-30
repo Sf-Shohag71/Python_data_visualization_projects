c2883525b4663c740fe90c162dddd46870528a89
Sf-Shohag71 <shakhfarid71@gmail.com>
2026-08-30 13:10:47 +0600

    Add dynamic title and location based on longitude and latitude

--- Earthquake_data_visualization/eq_explore_data.py
+++ Earthquake_data_visualization/eq_explore_data.py
@@ -6,22 +6,26 @@ import plotly.express as px
 path = Path('eq_data/eq_data_30_day_m1.geojson')
 contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
+data_title = all_eq_data['metadata']['title']
+# print(data_title)
 
 # Examine all earthquakes in the dataset.
 all_eq_dicts = all_eq_data['features']
 # print(len(all_eq_dicts))
 
-mags, lons, lats = [], [], []
+mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    eq_title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    eq_titles.append(eq_title)
 
 # Creating map
-fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title="Global Earthquake", color=mags, color_continuous_scale='Viridis',
-                     labels={'color': 'Magnitude'}, projection='natural earth'
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=data_title, color=mags, color_continuous_scale='mygbm',
+                     labels={'color': 'Magnitude'}, projection='natural earth', hover_name=eq_titles
                     )
 fig.show()

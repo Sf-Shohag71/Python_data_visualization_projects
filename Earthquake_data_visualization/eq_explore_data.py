ecc1d159043b0e5e43042d0c0561b643f854cc88
Sf-Shohag71 <shakhfarid71@gmail.com>
2026-08-30 12:25:18 +0600

    Basic visualization world's  earthquake possibilities using plotly(px.scatter_geo()) and python

---
+++ Earthquake_data_visualization/eq_explore_data.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import json
+import plotly.express as px
+
+# Read data as a string and convert to a Python object.
+path = Path('eq_data/eq_data_30_day_m1.geojson')
+contents = path.read_text(encoding='utf-8')
+all_eq_data = json.loads(contents)
+
+# Examine all earthquakes in the dataset.
+all_eq_dicts = all_eq_data['features']
+# print(len(all_eq_dicts))
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+# Creating map
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title="Global Earthquake", color=mags, color_continuous_scale='Viridis',
+                     labels={'color': 'Magnitude'}, projection='natural earth'
+                    )
+fig.show()
